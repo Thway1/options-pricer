58b1eeb700e0badaafd99ededf83a5c52c5580c6
Kyaw Zin Thant <kyawzinthant@Kyaws-MacBook-Pro.local>
2026-09-11 21:30:30 +0100

    Add docstrings and comments

--- src/black_scholes.py
+++ src/black_scholes.py
@@ -35,6 +35,17 @@ def delta_call_finite_diff(S, K, T, r, sigma, h=0.01):
     price_down= black_scholes_call(S - h, K, T, r, sigma)
     return (price_up-price_down)/(2*h)
 
+def gamma(S, K, T, r, sigma):
+    d1 = (np.log(S/K) + ((r+ 0.5 * sigma**2)*T)) / (sigma*np.sqrt(T))
+    return norm.pdf(d1) / (S * sigma * np.sqrt(T))
+
+def gamma_finite_diff(S, K, T, r, sigma, h=0.01):
+    price_up = black_scholes_call(S+h, K, T, r, sigma)
+    price_mid = black_scholes_call(S, K, T, r, sigma)
+    price_down = black_scholes_call(S-h, K, T, r, sigma)
+    return (price_up - 2 * price_mid + price_down) / (h**2)
+
+
 if __name__ == "__main__":
     call_price = black_scholes_call(100, 100, 1, 0.05, 0.2)
     print(f"Call Price: {call_price:.2f}")
@@ -45,9 +56,16 @@ if __name__ == "__main__":
     is_valid = check_call_put_parity(100, 100, 1, 0.05, 0.2)
     print(f"Parity Holds: {is_valid}")
 
-    print("\n---Data Check---")
+    print("\n---Delta Check---")
     analytical_delta = delta_call(100, 100, 1, 0.05, 0.2)
     numerical_delta = delta_call_finite_diff(100, 100, 1, 0.05, 0.2)
     print(f"Analytical Delta: {analytical_delta:.6f}")
     print(f"Numerical Delta: {numerical_delta:.6f}")
     print(f"Differenc: {abs(analytical_delta-numerical_delta):.10f}")
+
+    print("\n---Gamma Check---")
+    analytical_gamma = gamma(100, 100, 1, 0.05, 0.2)
+    numerical_gamma = gamma_finite_diff(100, 100, 1, 0.05, 0.2)
+    print (f"Analytical Gamma: {analytical_gamma:.6f}")
+    print (f"Numerical Gamma: {numerical_gamma:.6f}")
+    print (f"Difference; {abs(analytical_gamma - numerical_gamma):.10f}")
